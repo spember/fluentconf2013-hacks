e77f4a61f70111921d016bc9b0564826d717a850
Tim Wagner <tkw722@gmail.com>
2013-05-29 16:51:21 -0700

    The colors Duke, the colors\!

--- app/js/app.js
+++ app/js/app.js
@@ -6,13 +6,20 @@ $(function () {
         $server = $("input.server"),
         urlPattern = /(http|ftp|https):\/\/[\w\-]+(\.[\w\-]+)+([\w.,@?\^=%&amp;:\/~+#\-]*[\w@?\^=%&amp;\/~+#\-])?/,
         socket,
+        availableColors = ['DarkGray', 'Brown', 'DarkGreen', 'DarkSlateBlue', 'DarkSlateGray', 'IndianRed', 'LightSlateGray'],
+        userColorMap = {},
         buildMessage = function(data) {
             //pre process for links
             match = urlPattern.exec(data.text);
             if (match !== null) {
                 data.text = data.text.replace(match[0], "<a href='" + match[0] + "' target='_blank'>" + match[0] +"</a>");
             }
-            return '<div class="message ' + ($name.val() !== "" && data.text.indexOf($name.val()) > -1 ? 'alert' : '') +'"><span class="name">' +data.name +':</span>' + data.text + '</div>';
+
+            if(data.name && !userColorMap[data.name]) {
+                userColorMap[data.name] = availableColors.pop();
+            }
+
+            return '<div class="message group ' + ($name.val() !== "" && data.text.indexOf($name.val()) > -1 ? 'alert' : '') +'">' + (data.name ? '<div class="name ' + userColorMap[data.name] + '">' +data.name + '</div>' : '') + '<div class="text">' + data.text + '</div></div>';
         },
         scrollToBottom = function() {
             $messages[0].scrollTop = $messages[0].scrollHeight - $messages[0].offsetHeight;
@@ -28,7 +35,7 @@ $(function () {
 
     $input.on('keypress', function(event){
 
-        if(event.which == 13) {
+        if(event.which == 13 && $input.val()) {
             var text = $input.val(),
                 name = $name.val();
 
@@ -45,18 +52,28 @@ $(function () {
     });
 
     $server.on('keypress', function (event) {
+        var lastMessageName,
+            lastHistoryName;
         if (event.which === 13) {
             socket = io.connect('http://' + $server.val());
             localStorage.server = $server.val();
             socket.on("connect", function () {
                 socket.on('message', function (data) {
-                    $messages.append(buildMessage(data));
+                    $messages.append(buildMessage({
+                        name: (lastMessageName == data.name) ? null : data.name,
+                        text: data.text
+                    }));
+                    lastMessageName = data.name;
                     scrollToBottom();
                 }); 
                 socket.on('history', function (dataList) {
                     var messages = "";
                     for(var i = 0; i < dataList.length; i++) {
-                        messages += buildMessage(dataList[i]);
+                        messages += buildMessage({
+                            name: (lastHistoryName == dataList[i].name) ? null : dataList[i].name,
+                            text: dataList[i].text
+                        });
+                        lastHistoryName = dataList[i].name;
                     }
                     $messages.append(messages);
                     scrollToBottom();
