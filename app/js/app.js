93cc241e139e921f6a743e36827d0d14bcaf2c26
Steve Pember <spember@gmail.com>
2013-05-29 14:12:48 -0700

    -cache server and user name in localStorage

--- app/js/app.js
+++ app/js/app.js
@@ -12,6 +12,14 @@ $(function () {
             $messages[0].scrollTop = $messages[0].scrollHeight - $messages[0].offsetHeight;
         };
 
+    //look in localStorage for a previous name and server ip
+    if (localStorage.name !== undefined) { 
+        $name.val(localStorage.name);
+    }
+    if (localStorage.server !== undefined) {
+        $server.val(localStorage.server);
+    }
+
     $input.on('keypress', function(event){
 
         if(event.which == 13) {
@@ -21,6 +29,7 @@ $(function () {
             if(name === undefined || name === "") {
                 name = "Anonymous";
             }
+            localStorage.name = name;
             socket.emit('message', {
                 name: name,
                 text: text
@@ -32,7 +41,7 @@ $(function () {
     $server.on('keypress', function (event) {
         if (event.which === 13) {
             socket = io.connect('http://' + $server.val());
-            console.log("connecting on " +$server.val());
+            localStorage.server = $server.val();
             socket.on("connect", function () {
                 socket.on('message', function (data) {
                     $messages.append(buildMessage(data));
